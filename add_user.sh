225b16593a6d18f9275b9cde3774202ff994b6d6
Richard Chew <rchew@palantir.com>
2020-04-08 14:07:11 -0700

    Add script to add users

---
+++ add_user.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Script to add a user to Linux system
+if [ $(id -u) -eq 0 ]; then
+  if [ -z "${username}" ]; then
+    read -p "Enter username : " username
+  fi
+  if [ -z "${password}" ]; then
+    read -s -p "Enter password : " password
+    echo
+  fi
+  egrep "^$username" /etc/passwd >/dev/null
+  if [ $? -eq 0 ]; then
+      echo "${username} exists!"
+      exit 1
+  else
+  pass=$(perl -e 'print crypt($ARGV[0], "password")' "${password}")
+      useradd -m -p "${pass}" "${username}"
+      [ $? -eq 0 ] && echo "User has been added to system!" || echo "Failed to add a user!"
+  fi
+else
+  echo "Only root may add a user to the system"
+  exit 2
+fi
